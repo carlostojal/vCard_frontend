7bdda18f78dd9f61f6e4ff8144693e4a734d50a1
carlostojal <carlos.tojal@hotmail.com>
2023-11-05 19:34:04 +0000

    implement a notification service url util

--- vCard/src/utils/ConfigUtil.js
+++ vCard/src/utils/ConfigUtil.js
@@ -4,4 +4,8 @@ export default class ConfigUtil {
     static getApiUrl() {
         return `${import.meta.env.VITE_BACKEND_ADDRESS}:${import.meta.env.VITE_BACKEND_PORT}${import.meta.env.VITE_BACKEND_ENDPOINT}`;
     }
+
+    static getNotificationUrl(token) {
+        return `ws://${token}@${import.meta.env.VITE_NOTIFICATION_ADDRESS}:${import.meta.env.VITE_NOTIFICATION_PORT}${import.meta.env.VITE_NOTIFICATION_ENDPOINT}`;
+    }
 }
