a83f884b0281eebbbf87321af503b3930949c6ff
carlostojal <carlos.tojal@hotmail.com>
2023-11-09 11:38:07 +0000

    add user store

---
+++ vCard/src/stores/user.js
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+    state: () => ({
+        name: 'John Doe',
+        balance: 0
+    }),
+    actions: {
+        addBalance(amount) {
+            if(amount > 0)
+                this.balance += amount;
+        },
+        removeBalance(amount) {
+            if(amount > 0)
+                this.balance -= amount;
+        },
+        updateName(name) {
+            this.name = name;
+        }
+    }
+});
