ee89364198e39ed868ee9e5a0c9a43f7448cb0b8
carlostojal <carlos.tojal@hotmail.com>
2023-11-09 11:42:58 +0000

    get the user name using the pinia store on the homepage

--- vCard/src/views/home.vue
+++ vCard/src/views/home.vue
@@ -1,11 +1,17 @@
 <script setup>
 
+import Menu from '../components/menu.vue'
+import { useUserStore } from '@/stores/user'
+
+const user = useUserStore();
 
 </script>
 
 <template>
 
+    <Menu />
 
+    <h1>Hello, {{ user.name }}</h1>
 
 </template>
 
