10c4e9b8bc8fc02c7ae77d7023e81423551a5997
carlostojal <carlos.tojal@hotmail.com>
2023-11-17 10:48:20 +0000

    computed properties not needed

--- vCard/src/views/home.vue
+++ vCard/src/views/home.vue
@@ -1,53 +1,24 @@
 <script setup>
-
 import Menu from '../components/menu.vue'
 import vCard from '../components/vcard.vue'
-import { ref, computed } from 'vue'
-import axios from 'axios';
-
 import { useUserStore } from '@/stores/user'
 
-const user = useUserStore();
+const user = useUserStore()
 
-user.fetch().then(data => {
-    console.log(data);
-    user.updateName(data.name);
-}).catch(e => {
-    console.error("Error getting user data");
-});
-
-const userName = computed({
-    get: () => user.name,
-    set: (value) => user.updateName(value)
-});
-
-const userPhone = computed({
-    get: () => user.phone,
-    set: (value) => user.phone = value
-});
-
-const userBalance = computed({
-    get: () => user.balance,
-    set: (value) => user.balance = value
-});
-
-
-
+user.fetch().catch((e) => {
+  console.error('Error getting user data: ' + e)
+})
 </script>
 
 <template>
+  <Menu />
 
-    <Menu />
+  <div class="container">
+    <h1>Hello, {{ user.name }}</h1>
 
-    <div class="container">
-        <h1>Hello, {{ userName }}</h1>
-
-        <!-- TODO: the vCard component is not reacting to changes -->
-        <vCard :name="userName" :phone="userPhone" :balance="userBalance" />
-    </div>
-
+    <!-- TODO: the vCard component is not reacting to changes -->
+    <vCard :name="user.name" :phone="user.phone" :balance="user.balance" />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
