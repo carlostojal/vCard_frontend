e1974ad2f9b1b78e11f03a15bf23741741f5e0ea
carlostojal <carlos.tojal@hotmail.com>
2023-11-18 19:11:01 +0000

    list recent transactions on the home screen

--- vCard/src/views/home.vue
+++ vCard/src/views/home.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Menu from '../components/menu.vue'
 import vCard from '../components/vcard.vue'
-import transaction from '../components/transaction.vue'
+import Transaction from '../components/transaction.vue'
 import { useUserStore } from '@/stores/user'
+import { useTransactionsStore } from '@/stores/transactions'
 import FormatUtil from '../utils/FormatUtil'
 
-const user = useUserStore()
+const user = useUserStore();
+const transactions = useTransactionsStore();
 
 user.fetch().catch((e) => {
   console.error('Error getting user data: ' + e)
@@ -18,7 +20,7 @@ user.fetch().catch((e) => {
   <div class="container">
 
     <div class="header">
-      <h1>Hello, {{ FormatUtil.formatFirstLastName(user.name) }}</h1>
+      <h1 class="welcome">Hello, {{ FormatUtil.formatFirstName(user.name) }}.</h1>
 
       <!-- TODO: the vCard component is not reacting to changes -->
       <vCard :name="user.name" :phone="user.phone" :balance="user.balance" />
@@ -29,13 +31,18 @@ user.fetch().catch((e) => {
       <h2>Recent Transactions</h2>
 
       <div class="transactions-list">
-        <transaction v-for="transaction in user.transactions" :key="transaction.id" :type="transaction.type" :value="transaction.value" />
+        <Transaction v-for="transaction in transactions.getAll()" :key="transaction.id" :type="transaction.type" :value="transaction.value" :date="transaction.date" />
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+
+.welcome {
+  font-size: 3rem;
+  margin: 1rem 0 0.7rem 0;
+}
 
 .transactions {
   margin-top: 2rem;
