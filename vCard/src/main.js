64d50e1d4d1ac1ef97e119cad74df1f33b106ec2
carlostojal <carlos.tojal@hotmail.com>
2023-11-18 23:22:55 +0000

    init toast notifications

--- vCard/src/main.js
+++ vCard/src/main.js
@@ -2,6 +2,8 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css' 
 
 import "bootstrap/dist/css/bootstrap.min.css" 
 import "bootstrap" 
@@ -12,6 +14,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Toast)
 app.use(router)
 
 app.mount('#app')
